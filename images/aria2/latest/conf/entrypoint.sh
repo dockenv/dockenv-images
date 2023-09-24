d7eaec8deec6562a97a6d344f322e317fc0642ed
imxieke <oss@live.hk>
2023-09-25 07:48:43 +0800

    add seedbox qbittorrent

--- images/aria2/latest/conf/entrypoint.sh
+++ images/aria2/latest/conf/entrypoint.sh
@@ -2,7 +2,7 @@
 ###
  # @Author: Cloudflying
  # @Date: 2021-09-19 01:25:54
- # @LastEditTime: 2022-07-14 17:36:57
+ # @LastEditTime: 2023-09-24 21:50:52
  # @LastEditors: Cloudflying
  # @Description:
  # @FilePath: /dockenv/images/aria2/latest/conf/entrypoint.sh
@@ -12,16 +12,12 @@ if [[ -z "${ARIA2_PORT}" ]]; then
     ARIA2_PORT=6800
 fi
 
-if [[ -z "${WEBUI_PORT}" ]]; then
-    WEBUI_PORT=6801
-fi
-
 if [[ -z "${DHT_PORT}" ]]; then
-    DHT_PORT='6802-6999'
+    DHT_PORT='6881-6999'
 fi
 
 if [[ -z "${RPC_SECRET}" ]]; then
-    RPC_SECRET='haspwd'
+    RPC_SECRET='dockenv'
 fi
 
 sed -i "s/rpc-listen-port=.*/rpc-listen-port=${ARIA2_PORT}/g" ${ARIA2_CONF}
@@ -29,14 +25,11 @@ sed -i "s/rpc-secret=.*/rpc-secret=${RPC_SECRET}/g" ${ARIA2_CONF}
 sed -i "s/dht-listen-port.*/dht-listen-port=${DHT_PORT}/g" ${ARIA2_CONF}
 
 aria2c --conf-path=/etc/aria2c/aria2c.conf -D
-darkhttpd /opt/ariang --port ${WEBUI_PORT} --daemon >>/dev/null
 echo -e "
-    Welcome to Aria2c Container is Running
+    Welcome to Use Aria2c Container
 
 Aria2 Port : 127.0.0.1:${ARIA2_PORT}
-WEBUI Port : 127.0.0.1:${WEBUI_PORT}
 RPC Secret : ${RPC_SECRET}
-
 "
 touch /var/log/aria2.log
 tail -f /var/log/aria2.log
