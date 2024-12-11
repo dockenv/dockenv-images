e713c4143eac5dd01437542c3ff1568ebead6f7e
imxieke <oss@live.hk>
2024-12-11 13:52:28 +0800

    update shadowsocks

--- images/shadowsocks/latest/entrypoint.sh
+++ images/shadowsocks/latest/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
-sed -i "s#PORT#${PORT}#g" /tmp/shadowsocks.json
-sed -i "s#PASSWD#${PASSWD}#g" /tmp/shadowsocks.json
-sed -i "s#METHOD#${METHOD}#g" /tmp/shadowsocks.json
-exec ss-server -c /tmp/shadowsocks.json -u
+
+[ -n "$PORT" ] && sed -i "s#6443#${PORT}#g" /etc/shadowsocks.json
+[ -n "$METHOD" ] && sed -i "s#chacha20-ietf-poly1305#${METHOD}#g" /etc/shadowsocks.json
+[ -n "$PASSWD" ] && sed -i "s#dockpass#${PASSWD}#g" /etc/shadowsocks.json
+
+exec ssserver -c /etc/shadowsocks.json
