ab51f1401fe50837c4666bd8e7d3d38592282897
imxieke <oss@live.hk>
2025-12-12 18:26:08 +0800

    update code-server

---
+++ images/code-server/latest/entrypoint.sh
@@ -0,0 +1,29 @@
+#!/usr/bin/env bash
+###
+ # @Author: Cloudflying
+ # @Date: 2025-12-12 16:58:06
+ # @LastEditTime: 2025-12-12 18:25:00
+ # @LastEditors: Cloudflying
+ # @Description: Code Server Entrypoint
+###
+
+RANDOM_PASSWORD=$(tr -dc A-Za-z0-9 < /dev/urandom | head -c 8)
+
+DEFAULT_PORT=${DEFAULT_PORT:-8080}
+DEFAULT_USER=${DEFAULT_USER:-dockenv}
+DEFAULT_PASSWD=${DEFAULT_PASSWD:-$RANDOM_PASSWORD}
+CONFIG_FILE="/home/${DEFAULT_USER}/.config/code-server/config.yaml"
+CONFIG_FILE="./logs"
+
+
+# if [[ -d "/home/${DEFAULT_USER}" ]]; then
+mkdir -p "/home/${DEFAULT_USER}/.config/code-server"
+
+echo -E "bind-addr:0.0.0.0:${DEFAULT_PORT}
+auth: password
+password: ${DEFAULT_PASSWD}
+cert: false" > "${CONFIG_FILE}"
+
+chown -R "${DEFAULT_USER}:${DEFAULT_USER}" "/home/${DEFAULT_USER}/.config"
+
+exec code-server
