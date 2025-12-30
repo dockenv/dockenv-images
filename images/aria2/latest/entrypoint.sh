06473f7d44eb259b7de6e490139c6857f8137937
imxieke <oss@live.hk>
2025-12-30 15:07:18 +0800

    update aria2 conf permission

--- images/aria2/latest/entrypoint.sh
+++ images/aria2/latest/entrypoint.sh
@@ -2,7 +2,7 @@
 ###
 # @Author: Cloudflying
 # @Date: 2021-09-19 01:25:54
- # @LastEditTime: 2025-12-30 14:58:28
+ # @LastEditTime: 2025-12-30 15:06:19
  # @LastEditors: Cloudflying
 # @Description:
 ###
@@ -27,11 +27,6 @@ if [ -z "${RPC_SECRET}" ]; then
   RPC_SECRET='dockpass'
 fi
 
-touch /data/logs/aria2c.log
-adduser -D -u "${UID}" -s /bin/sh "${USER}"
-chown -R "${USER}:${GROUP}" /data
-chmod 755 -R /data
-
 if [ ! -f "/etc/aria2c/aria2c.conf" ] ; then
   echo "Aria2 Conf Not Found, Exiting..."
   exit 1
@@ -40,6 +35,13 @@ fi
 if [ ! -f "/etc/aria2c/aria2c.session" ] ; then
   touch /etc/aria2c/aria2c.session
 fi
+
+touch /data/logs/aria2c.log
+adduser -D -u "${UID}" -s /bin/sh "${USER}"
+chown -R "${USER}:${GROUP}" /data
+chown -R "${USER}:${GROUP}" /etc/aria2c
+chmod 755 -R /data
+chmod 755 -R /etc/aria2c
 
 sed -i "s/rpc-listen-port=.*/rpc-listen-port=${ARIA2_PORT}/g" ${ARIA2_CONF}
 sed -i "s/rpc-secret=.*/rpc-secret=${RPC_SECRET}/g" ${ARIA2_CONF}
